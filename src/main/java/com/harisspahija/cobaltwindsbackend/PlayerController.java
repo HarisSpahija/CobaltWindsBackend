f95970556758c0f74a0253fb718e8509f6c68021
HarisSpahija <harisspahija@gmail.com>
2023-01-13 17:21:33 +0100

    Added usages of all enums

--- src/main/java/com/harisspahija/cobaltwindsbackend/PlayerController.java
+++ src/main/java/com/harisspahija/cobaltwindsbackend/PlayerController.java
@@ -8,10 +8,16 @@ import java.util.ArrayList;
 @RestController
 public class PlayerController {
     ArrayList<Player> players = new ArrayList<>();
-    Player samplePlayer = new Player("Haris", LocalDate.of(1996, 11, 30), Role.Top, Role.Jungle, "https://www.op.gg/summoners/euw/Beedle", true);
+    Player sampleTopPlayer = new Player("Haris", LocalDate.of(1996, 11, 30), Role.Top, Role.Jungle, "https://www.op.gg/summoners/euw/Beedle", true);
+    Player sampleMiddlePlayer = new Player("Haris", LocalDate.of(1996, 11, 30), Role.Middle, Role.ADC, "https://www.op.gg/summoners/euw/Beedle", true);
+    Player sampleBotPlayer = new Player("Haris", LocalDate.of(1996, 11, 30), Role.Support, Role.ADC, "https://www.op.gg/summoners/euw/Beedle", true);
+    Player sampleFillPlayer = new Player("Haris", LocalDate.of(1996, 11, 30), Role.Fill, Role.Support, "https://www.op.gg/summoners/euw/Beedle", true);
 
     public PlayerController() {
-        this.players.add(samplePlayer);
+        this.players.add(sampleTopPlayer);
+        this.players.add(sampleMiddlePlayer);
+        this.players.add(sampleBotPlayer);
+        this.players.add(sampleFillPlayer);
     }
 
     @GetMapping("/players")
