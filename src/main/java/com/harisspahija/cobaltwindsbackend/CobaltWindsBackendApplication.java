f4c7cfa4d7e7ffc0e51a14760872b3c5ef69490a
HarisSpahija <harisspahija@gmail.com>
2023-01-13 13:21:57 +0100

    Init

---
+++ src/main/java/com/harisspahija/cobaltwindsbackend/CobaltWindsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.harisspahija.cobaltwindsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobaltWindsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobaltWindsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harisspahija/cobaltwindsbackend/CobaltWindsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harisspahija.cobaltwindsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobaltWindsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
