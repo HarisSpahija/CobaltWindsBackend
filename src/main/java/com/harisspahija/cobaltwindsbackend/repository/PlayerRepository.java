5932ca09b9c3b4a575a46f42c80fdd8f9c321fc8
HarisSpahija <harisspahija@gmail.com>
2023-01-15 03:46:42 +0100

    Created new Player Repository

---
+++ src/main/java/com/harisspahija/cobaltwindsbackend/repository/PlayerRepository.java
@@ -0,0 +1,10 @@
+package com.harisspahija.cobaltwindsbackend.repository;
+
+import com.harisspahija.cobaltwindsbackend.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PlayerRepository extends JpaRepository<Player, String> {
+    List<Player> findByFreeAgent(boolean freeAgent);
+}
