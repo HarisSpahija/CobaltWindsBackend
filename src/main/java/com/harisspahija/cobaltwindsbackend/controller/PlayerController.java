04bb6ad286f8b265532c603a52a7ba3d3603e6f5
HarisSpahija <harisspahija@gmail.com>
2023-01-15 04:05:42 +0100

    Refactored player to use repository

---
+++ src/main/java/com/harisspahija/cobaltwindsbackend/controller/PlayerController.java
@@ -0,0 +1,95 @@
+package com.harisspahija.cobaltwindsbackend.controller;
+
+import com.harisspahija.cobaltwindsbackend.model.Player;
+import com.harisspahija.cobaltwindsbackend.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PlayerController {
+    @Autowired
+    PlayerRepository playerRepository;
+
+    @GetMapping("/players")
+    public ResponseEntity<List<Player>> getAllPlayers() {
+        try {
+            List<Player> players = new ArrayList<>(playerRepository.findAll());
+
+            return new ResponseEntity<>(players, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/players/free-agents")
+    public ResponseEntity<List<Player>> getAllFreeAgents() {
+        try {
+            List<Player> players = playerRepository.findByFreeAgent(true);
+
+            if (players.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(players, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/players/{id}")
+    public ResponseEntity<Object> getPlayer(@PathVariable String id) {
+        Optional<Player> playerData = playerRepository.findById(id);
+
+        if (playerData.isPresent()) {
+            return new ResponseEntity<>(playerData.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Player not found with id: " + id, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/players")
+    public ResponseEntity<Object> createPlayer(@RequestBody Player player) {
+        try {
+            Player _player = playerRepository.save(new Player(player.getName(), player.getDateOfBirth(), player.getPrimaryRole(), player.getSecondaryRole(), player.getOpggLink(), player.getFreeAgent()));
+            return new ResponseEntity<>(_player, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/players/{id}")
+    public ResponseEntity<Object> updatePlayer(@PathVariable("id") String id, @RequestBody Player player) {
+        if (player.hasDuplicateRole())
+            return new ResponseEntity<>("player can not have duplicate roles", HttpStatus.BAD_REQUEST);
+
+        Optional<Player> playerData = playerRepository.findById(id);
+
+        if (playerData.isPresent()) {
+            Player _player = playerData.get();
+            _player.setName(player.getName());
+            _player.setDateOfBirth(player.getDateOfBirth());
+            _player.setPrimaryRole(player.getPrimaryRole());
+            _player.setSecondaryRole(player.getSecondaryRole());
+            _player.setOpggLink(player.getOpggLink());
+            _player.setFreeAgent(player.getFreeAgent());
+
+            return new ResponseEntity<>(playerRepository.save(_player), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Player not found with id: " + id, HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/players/{id}")
+    public ResponseEntity<Object> deletePlayer(@PathVariable("id") String id) {
+        Optional<Player> playerData = playerRepository.findById(id);
+
+        if (playerData.isPresent()) {
+            playerRepository.delete(playerData.get());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>("Player not found with id: " + id, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/harisspahija/cobaltwindsbackend/model/Player.java
@@ -0,0 +1,98 @@
+package com.harisspahija.cobaltwindsbackend.model;
+
+import com.harisspahija.cobaltwindsbackend.Role;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Players")
+public class Player {
+    @Id
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "primary_role")
+    private Role primaryRole;
+
+    @Column(name = "secondary_role")
+    private Role secondaryRole;
+
+    @Column(name = "opgg_link")
+    private String opggLink;
+
+    @Column(name = "free_agent")
+    private boolean freeAgent;
+
+    public Player() {
+    }
+
+    public Player(String name, LocalDate dateOfBirth, Role primaryRole, Role secondaryRole, String opggLink, Boolean freeAgent) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.primaryRole = primaryRole;
+        this.secondaryRole = secondaryRole;
+        this.opggLink = opggLink;
+        this.freeAgent = freeAgent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Role getPrimaryRole() {
+        return primaryRole;
+    }
+
+    public void setPrimaryRole(Role primaryRole) {
+        this.primaryRole = primaryRole;
+    }
+
+    public Boolean hasDuplicateRole() {
+        return this.primaryRole == this.secondaryRole;
+    }
+
+    public Role getSecondaryRole() {
+        return secondaryRole;
+    }
+
+    public void setSecondaryRole(Role secondaryRole) {
+        this.secondaryRole = secondaryRole;
+    }
+
+    public String getOpggLink() {
+        return opggLink;
+    }
+
+    public void setOpggLink(String opggLink) {
+        this.opggLink = opggLink;
+    }
+
+    public Boolean getFreeAgent() {
+        return freeAgent;
+    }
+
+    public void setFreeAgent(Boolean freeAgent) {
+        this.freeAgent = freeAgent;
+    }
+}
