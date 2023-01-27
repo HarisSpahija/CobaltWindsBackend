85b23d3c98bd0c48837df5b0f82b6951492142f1
HarisSpahija <harisspahija@gmail.com>
2023-01-27 13:59:48 +0100

    Removed unused exceptionhandler

--- src/main/java/com/harisspahija/cobaltwindsbackend/advice/PlayerExceptionAdvice.java
+++ src/main/java/com/harisspahija/cobaltwindsbackend/advice/PlayerExceptionAdvice.java
@@ -3,7 +3,6 @@ package com.harisspahija.cobaltwindsbackend.advice;
 import com.harisspahija.cobaltwindsbackend.exception.PlayerHasDuplicateRoleException;
 import com.harisspahija.cobaltwindsbackend.exception.PlayerHasPrimaryRoleAndSecondaryRoleIsNullException;
 import com.harisspahija.cobaltwindsbackend.exception.PlayerHasPrimaryRoleFillAndSecondaryRoleNotNullException;
-import com.harisspahija.cobaltwindsbackend.exception.PlayerNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,14 +11,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class PlayerExceptionAdvice {
-
-    @ResponseBody
-    @ExceptionHandler(PlayerNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    String playerNotFoundHandler(PlayerNotFoundException ex) {
-        return ex.getMessage();
-    }
-
     @ResponseBody
     @ExceptionHandler(PlayerHasDuplicateRoleException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
