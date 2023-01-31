b4b5348e5219e4c50768c34895cdbd255ff8727a
HarisSpahija <harisspahija@gmail.com>
2023-01-31 03:19:13 +0100

    Fixed proper Path Matching

--- src/main/java/com/harisspahija/cobaltwindsbackend/security/SecurityConfig.java
+++ src/main/java/com/harisspahija/cobaltwindsbackend/security/SecurityConfig.java
@@ -51,20 +51,22 @@ public class SecurityConfig  {
                 .httpBasic().disable()
                 .authorizeHttpRequests()
 
+                // Teams
+                .requestMatchers(HttpMethod.POST, "/teams").hasAuthority("PLAYER")
+                .requestMatchers(HttpMethod.POST,"/teams/**/join").hasAuthority("PLAYER")
+                .requestMatchers(HttpMethod.PUT, "/teams/*").hasAnyAuthority("TEAM_CAPTAIN", "ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/teams/*").hasAnyAuthority("TEAM_CAPTAIN", "ADMIN")
+                // Players
                 .requestMatchers(HttpMethod.POST, "/players").hasAuthority("USER")
                 .requestMatchers(HttpMethod.PUT, "/players/*").hasAnyAuthority("USER", "ADMIN")
                 .requestMatchers(HttpMethod.DELETE, "/players/*").hasAnyAuthority("USER", "ADMIN")
-                .requestMatchers(HttpMethod.POST, "/teams").hasAuthority("PLAYER")
-                .requestMatchers(HttpMethod.POST,"/teams/*/join").hasAuthority("PLAYER")
-                .requestMatchers(HttpMethod.PUT, "/teams/**").hasAnyAuthority("TEAM_CAPTAIN", "ADMIN")
-                .requestMatchers(HttpMethod.DELETE, "/teams/**").hasAnyAuthority("TEAM_CAPTAIN", "ADMIN")
                 // Roles Admin
-                .requestMatchers(HttpMethod.GET, "/roles/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/roles/*").hasAuthority("ADMIN")
                 .requestMatchers(HttpMethod.PUT, "/roles/*").hasAuthority("ADMIN")
                 // Users Admin
                 .requestMatchers(HttpMethod.GET, "/users").hasAuthority("ADMIN")
-                .requestMatchers(HttpMethod.PUT, "/users/**").hasAuthority("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/users/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/users/*").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/users/*").hasAuthority("ADMIN")
                 // Users Me
                 .requestMatchers(HttpMethod.GET, "/users/me").hasAuthority("USER")
                 .requestMatchers(HttpMethod.PUT, "/users/me").hasAuthority("USER")
@@ -73,7 +75,9 @@ public class SecurityConfig  {
                 // Auth
                 .requestMatchers(HttpMethod.POST, "/auth").permitAll()
                 // Public routes
+                .requestMatchers(HttpMethod.GET, "/players/*").permitAll()
                 .requestMatchers(HttpMethod.GET, "/*").permitAll()
+
                 .and()
                 .addFilterBefore(new JwtRequestFilter(jwtService, userDetailsService()), UsernamePasswordAuthenticationFilter.class)
                 .csrf().disable()
